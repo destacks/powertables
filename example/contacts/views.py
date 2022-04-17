791c3bfd358b451231a2e0e53b2f1ce98049e2c9
Alexis Michaltsis <a.michaltsis@gmail.com>
2022-04-17 16:06:03 +0200

    Add nested queries with Q query class

--- example/contacts/views.py
+++ example/contacts/views.py
@@ -1,3 +1,7 @@
+from functools import lru_cache
+from typing import List
+
+from django.db.models import Q
 from django.forms import modelform_factory
 from django.views.generic import ListView
 from django.views.generic.edit import FormMixin
@@ -25,18 +29,48 @@ class PowerTableView(FormMixin, ListView):
             kwargs.update({"data": self.request.GET})
         return kwargs
 
+    @lru_cache(maxsize=1)
+    def get_field_value_list(self, field_name: str) -> List:
+        form = self.get_form()
+        return [item for item in form.data.getlist(field_name) if item]
+
     def get_queryset(self):
         queryset = super().get_queryset()
-        form = self.get_form()
-        kwargs = {
-            f"{field.column}__in": [
-                item for item in form.data.getlist(field.name) if item
-            ]
+        fields = [
+            {"field": field, "value_list": self.get_field_value_list(field.name)}
             for field in get_model_fields(self.model)
-            if [item for item in form.data.getlist(field.name) if item]
-        }
-        fields = get_model_fields(self.model, option="name")
-        return queryset.filter(**kwargs).order_by(*fields)
+            if self.get_field_value_list(field.name)
+        ]
+        field_names = [field["field"].name for field in fields]
+        query = None
+        for field in fields:
+            value_list = field["value_list"]
+            field_item = field["field"]
+            has_icontains = (
+                True if "icontains" in field_item.class_lookups.keys() else False
+            )
+            subquery = None
+
+            for value in value_list:
+                if has_icontains:
+                    kwarg = {f"{field_item.name}__icontains": value}
+                else:
+                    kwarg = {f"{field_item.column}": value}
+                if subquery is None:
+                    subquery = Q(**kwarg)
+                else:
+                    subquery.add(Q(**kwarg), Q.OR)
+
+            if subquery is not None:
+                if query is None:
+                    query = subquery
+                else:
+                    query.add(subquery, Q.AND)
+
+        if query is None:
+            return queryset.order_by(*field_names)
+        else:
+            return queryset.filter(query).order_by(*field_names)
 
 
 class ContactPowerTableView(PowerTableView):
