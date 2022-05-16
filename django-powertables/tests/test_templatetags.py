5b1b93efae8b9d00b1d6c8d6f86de7a729166463
Alexis Michaltsis <a.michaltsis@gmail.com>
2022-05-16 15:02:39 +0200

    Update formatting

--- django-powertables/tests/test_templatetags.py
+++ django-powertables/tests/test_templatetags.py
@@ -1,10 +1,8 @@
 from django.test import TestCase
-
 from powertables.templatetags.powertable_tags import with_sorting_prefix
 
 
 class PowertableTemplatetagsTest(TestCase):
-
     def test_that_with_sorting_prefix_returns_correct_value(self):
         sorting_foo = with_sorting_prefix("foo")
         self.assertEqual(sorting_foo, "sorting_foo")
